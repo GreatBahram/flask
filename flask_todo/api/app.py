a50211cc1bd2045316ef1bf356e269c14cd6a5f5
GreatBahram <aghaee.bahram@gmail.com>
2018-08-15 15:23:00 +0430

    improvement on apis

--- flask_todo/api/app.py
+++ flask_todo/api/app.py
@@ -21,21 +21,6 @@ def forbidden_response():
     )
     return response
 
-
-def notfound_response():
-    """Return an HTTP response when a nonexistant profile has been searched for."""
-    response = Response(
-        mimetype="application/json",
-        response=json.dumps({'error': 'The profile does not exist'}),
-        status=404
-    )
-    return response
-
-def get_profile(username):
-    """Check if the requested profile exists."""
-    return Profile.query.filter_by(username=username).first()
-
-
 @auth.verify_token
 def verify_token(token):
     """Verify that the incoming request has the expected token."""
@@ -51,70 +36,9 @@ def authenticate(response, profile):
     response.set_cookie('auth_token', value=token)
     return response
 
-
-@app.route('/api/v1/accounts/<username>/tasks', methods=['GET'])
-@auth.login_required
-def task_list(username):
-    """List all of the tasks for one user."""
-    profile = get_profile(username)
-    if profile:
-        tasks = [task.to_dict() for task in profile.tasks.all()]
-        output = {'username': username, 'tasks': tasks}
-        response = Response(
-            mimetype="application/json",
-            response=json.dumps(output)
-        )
-        return authenticate(response, profile)
-    return notfound_response()
-
-
-@app.route('/api/v1/accounts/<username>/tasks', methods=['POST'])
-@auth.login_required
-def create_task(username):
-    """List all of the tasks for one user."""
-    profile = get_profile(username)
-    if profile:
-        task = Task(
-            name=request.form['name'],
-            note=request.form['note'],
-            creation_date=datetime.now(),
-            due_date=datetime.strptime(due_date, INCOMING_DATE_FMT) if due_date else None,
-            completed=request.form['completed'],
-            profile_id=profile.id,
-        )
-        db.session.add(task)
-        db.session.commit()
-        output = {'msg': 'posted'}
-        response = Response(
-            mimetype="application/json",
-            response=json.dumps(output),
-            status=201
-        )
-        return authenticate(response, profile)
-    return notfound_response()
-
-
-@app.route('/api/v1/accounts/<username>/tasks/<int:task_id>', methods=['GET'])
-@auth.login_required
-def task_detail(username, task_id):
-    """Get the detail for one task if that task belongs to the provided user."""
-    profile = get_profile(username)
-    if profile:
-        task = Task.query.get(task_id)
-        if task in profile.tasks:
-            output = {'username': username, 'task': task.to_dict()}
-            response = Response(
-                mimetype="application/json",
-                response=json.dumps(output)
-            )
-            return authenticate(response, profile)
-    return notfound_response()
-
-
 @app.route('/api/v1/accounts/<username>/tasks/<int:task_id>', methods=['PUT'])
 @auth.login_required
 def task_update(username, task_id):
-    """Update one task if that task belongs to the provided user."""
     profile = get_profile(username)
     if profile:
         task = Task.query.get(task_id)
@@ -142,7 +66,6 @@ def task_update(username, task_id):
 @app.route('/api/v1/accounts/<username>/tasks/<int:task_id>', methods=['GET'])
 @auth.login_required
 def task_delete(username, task_id):
-    """Delete one task if that task belongs to the provided user."""
     profile = get_profile(username)
     if profile:
         task = Task.query.get(task_id)
--- flask_todo/resources/task.py
+++ flask_todo/resources/task.py
@@ -11,23 +11,21 @@ INCOMING_DATE_FMT = '%d/%m/%Y %H:%M:%S'
 class TaskList(Resource):
     @jwt_required()
     def get(self):
+        """List all of the tasks for one user."""
         username = current_identity
-        return {'Tasks': username.tasks}
+        return {'Tasks': [task.to_dict()for task in username.tasks]}
 
 
 class CreateTask(Resource):
     @jwt_required()
     def post(self):
         username = current_identity
-        print(username)
-
         parser = reqparse.RequestParser()
         parser.add_argument('name', required=True, help="This field cannot be blank!")
         parser.add_argument('note', required=True, help="This field cannot be blank!")
         parser.add_argument('due-date', help="This field cannot be blank!")
         parser.add_argument('completed', help="This field cannot be blank!")
         data = parser.parse_args()
-
         try:
             task = TaskModel(name=data['name'], note=data['note'],user=username) 
             task.save_to_db()
@@ -35,22 +33,27 @@ class CreateTask(Resource):
         except:
             return {'message': 'Something wrong happend.'}, 400
 
+
 class Task(Resource):
+    @jwt_required()
     def get(self, task_id):
-        """Get the detail for an individual profile."""
-        task = TaskModel.find_by_id(task_id)
-        if task:
+        """Get the detail for one task if that task belongs to the provided user."""
+        task = TaskModel.query.filter_by(id=task_id).first()
+        current_user_tasks = current_identity.tasks
+        if task and task in current_user_tasks:
             return task.to_dict()
         return {'message': 'Task not found'}, 404
 
     def put(self, task_id):
+        """Update one task if that task belongs to the provided user."""
         pass
 
+    @jwt_required()
     def delete(self, task_id):
-        """."""
+        """Delete one task if that task belongs to the provided user."""
         task = TaskModel.query.filter_by(id=task_id).first()
-        if task:
+        current_user_tasks = current_identity.tasks
+        if task and task in current_user_tasks:
             task.delete_from_db()
             return {'message': f'Task {task_id} has been deleted.'}, 201
         return {'message': 'Task not found'}, 404
-        
